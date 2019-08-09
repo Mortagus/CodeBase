68a4431a1f7ef6acb56058db2345cb8141a63690
Benjamin Lemin <benjamin.lemin@gmail.com>
2019-08-09 13:10:13 +0200

    First Commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     *
+     * @param Environment $twig
+     *
+     * @return Response
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index(Environment $twig)
+    {
+        return new Response($twig->render('default/default.html.twig'));
+    }
+}
